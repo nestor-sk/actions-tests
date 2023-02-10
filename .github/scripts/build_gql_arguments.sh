d79a2f0dc1a7ff1db53e0334814845e082415de0
Nestor Lafon Gracia <nestor@sketch.com>
2023-02-10 17:26:44 +0100

    update

--- .github/scripts/build_gql_arguments.sh
+++ .github/scripts/build_gql_arguments.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# Usage: bash build_gql_arguments.sh 'schema 1','schema 2' 'query 1','query 2' 'ignore 1','ignore 2'
-# Prints --schema schema1 --schema schema2 --ignore ignore1 --ignore ignore2 query1 query2
+# Usage: bash build_gql_arguments.sh schema 1,schema 2 query 1,query 2 ignore 1,ignore 2
+# Prints --schema "schema1" --schema "schema2" --ignore "ignore1" --ignore "ignore2" "query1" "query2"
 
 set -f # disable globbing
 
-schema_args=$(echo $1 | sed 's/,/ --schema /g')
+schema_args=$(echo $1 | sed 's/,/ /g')
 query_args=$(echo $2 | sed 's/,/ /g')
-ignore_args=$(echo $3 | sed 's/,/ --ignore /g')
+ignore_args=$(echo $3 | sed 's/,/ /g')
 
-if [ -z "$ignore_args" ]; then
-    ignore_args=""
-else
-    ignore_args="--ignore $ignore_args"
-fi
+for schema in $schema_args; do
+  echo -n "--schema \"$schema\" "
+done
 
-echo "--schema $schema_args $ignore_args $query_args"
+for ignore in $ignore_args; do
+  echo -n "--ignore \"$ignore\" "
+done
 
-
+for query in $query_args; do
+  echo -n "\"$query\" "
+done
 
