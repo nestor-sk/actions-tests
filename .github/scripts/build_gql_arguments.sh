bbe46c55b854d11910d1bf89c205753b5f758fb3
Nestor Lafon Gracia <nestor@sketch.com>
2023-02-10 17:31:33 +0100

    update

--- .github/scripts/build_gql_arguments.sh
+++ .github/scripts/build_gql_arguments.sh
@@ -2,8 +2,6 @@
 # Usage: bash build_gql_arguments.sh schema 1,schema 2 query 1,query 2 ignore 1,ignore 2
 # Prints --schema "schema1" --schema "schema2" --ignore "ignore1" --ignore "ignore2" "query1" "query2"
 
-set -f # disable globbing
-
 schema_args=$(echo $1 | sed 's/,/ /g')
 query_args=$(echo $2 | sed 's/,/ /g')
 ignore_args=$(echo $3 | sed 's/,/ /g')
